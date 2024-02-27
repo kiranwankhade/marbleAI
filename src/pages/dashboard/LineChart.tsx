b51684f6d79138258e3909e95ccc5d0d73c4c218
kiranwankhade <kiranwankhade7738@gamil.com>
2024-02-27 21:22:29 +0530

    add date picker

--- src/pages/dashboard/LineChart.tsx
+++ src/pages/dashboard/LineChart.tsx
@@ -21,6 +21,11 @@ import { MdKeyboardArrowUp, MdKeyboardArrowDown } from "react-icons/md";
 
 import { BsQuestionCircle } from "react-icons/bs";
 
+import { DateRangePicker } from "react-date-range";
+import Modal from "react-modal";
+import "react-date-range/dist/styles.css"; // main css file
+import "react-date-range/dist/theme/default.css"; // theme css file
+
 type TStats = {
   dailyRevenue?: GetListResponse<IChartDatum>;
   dailyOrders?: GetListResponse<IChartDatum>;
@@ -34,9 +39,65 @@ const LineChart = ({
   newCustomers,
   total,
 }: TStats) => {
+  const [showbg1, setShowbg1] = useState(false);
+  const [showbg2, setShowbg2] = useState(false);
+  const [showbg3, setShowbg3] = useState(false);
+
+  const [showOptions1, setShowOptions1] = useState(false);
+  const [showOptions2, setShowOptions2] = useState(false);
+  const [showOptions3, setShowOptions3] = useState(false);
+  const [showOptions4, setShowOptions4] = useState(false);
+
+  const [showChart, setShowChart] = useState(true);
+  const [showModal, setShowModal] = useState(false); // State for controlling modal visibility
+
+  const openModal = () => {
+    setShowModal(true);
+  };
+
+  const closeModal = () => {
+    setShowModal(false);
+  };
+
+  const [selectedDateRange, setSelectedDateRange] = useState([
+    {
+      startDate: new Date(new Date().getTime() - 7 * 24 * 60 * 60 * 1000), // 7 days ago
+      endDate: new Date(),
+      key: "selection",
+    },
+  ]);
+
+  const handleDateRangeChange = (ranges: any) => {
+    setSelectedDateRange([ranges.selection]);
+  };
+
+  const filteredRevenueData = dailyRevenue?.filter((item: any) => {
+    const date = new Date(item.date);
+    return (
+      date >= selectedDateRange[0].startDate &&
+      date <= selectedDateRange[0].endDate
+    );
+  });
+
+  const filteredDailyOrdersData = dailyOrders?.filter((item: any) => {
+    const date = new Date(item.date);
+    return (
+      date >= selectedDateRange[0].startDate &&
+      date <= selectedDateRange[0].endDate
+    );
+  });
+
+  const filteredNewCustomersData = newCustomers?.filter((item: any) => {
+    const date = new Date(item.date);
+    return (
+      date >= selectedDateRange[0].startDate &&
+      date <= selectedDateRange[0].endDate
+    );
+  });
+
   // Function to calculate total value
   const calculateTotal = (data: any) => {
-    return data.reduce((acc: any, item: any) => acc + item.value, 0);
+    return data?.reduce((acc: any, item: any) => acc + item.value, 0);
   };
 
   // Calculate total values
@@ -53,7 +114,6 @@ const LineChart = ({
     0
   );
 
-
   const netReturnValue = totalRevenue - netCosts;
 
   const totalOrders = dailyOrders?.reduce(
@@ -68,18 +128,36 @@ const LineChart = ({
 
   const conversionRate = totalOrders / onlineStoreSessions;
 
+
+  // Calculate filter total values
+  const filteredTotalNewCustomers = calculateTotal(filteredNewCustomersData);
+  const filteredTotalDailyOrders = calculateTotal(filteredDailyOrdersData);
+  const filteredTotalDailyRevenue = calculateTotal(filteredDailyOrdersData);
+
+
+   // Calculate net cost
+   const filteredTotalExpenses = totalNewCustomers + totalDailyOrders;
+   const filteredNetCosts = totalDailyRevenue - totalExpenses;
  
+   const filteredTotalRevenue: number = dailyRevenue?.reduce(
+     (acc: Number, curr: any) => acc + curr?.value,
+     0
+   );
+ 
+   const filteredTNetReturnValue = filteredTotalRevenue - filteredNetCosts;
+ 
+   const filteredTotalOrders = filteredDailyOrdersData?.reduce(
+     (acc: Number, curr: any) => acc + curr?.value,
+     0
+   );
+ 
+   const filteredOnlineStoreSessions = filteredNewCustomersData?.reduce(
+     (acc: Number, curr: any) => acc + curr?.value,
+     0
+   );
+ 
+   const filteredConversionRate = filteredTotalOrders / filteredOnlineStoreSessions;
 
-  const [showbg1, setShowbg1] = useState(false);
-  const [showbg2, setShowbg2] = useState(false);
-  const [showbg3, setShowbg3] = useState(false);
-
-  const [showOptions1, setShowOptions1] = useState(false);
-  const [showOptions2, setShowOptions2] = useState(false);
-  const [showOptions3, setShowOptions3] = useState(false);
-  const [showOptions4, setShowOptions4] = useState(false);
-
-  const [showChart, setShowChart] = useState(true);
 
   const handlePencilClick = (divNumber: Number) => {
     if (divNumber === 1) {
@@ -116,7 +194,7 @@ const LineChart = ({
     return () => {
       document.removeEventListener("mousedown", handleClickOutside);
     };
-  }, []);
+  }, [selectedDateRange]);
 
   const [hoverStates, setHoverStates] = useState([
     false,
@@ -144,7 +222,38 @@ const LineChart = ({
 
   return (
     <div className="line-chart p-2 bg-white border rounded-lg">
-      {/* <h2>Loading Line Chart...</h2> */}
+     <div className="mb-3">
+        <button onClick={openModal}>Open Date Range Picker</button>
+        {/* Modal for Date Range Picker */}
+        <Modal
+          isOpen={showModal}
+          onRequestClose={closeModal}
+          contentLabel="Date Range Modal"
+          style={{
+            content: {
+              zIndex: 10, // Custom z-index
+              top: "55%",
+              left: "50%",
+              right: "auto",
+              bottom: "auto",
+              marginRight: "-50%",
+              transform: "translate(-50%, -50%)", // Center modal
+            },
+          }}
+        >
+          <div className="flex justify-end">
+            <button onClick={closeModal}>Close</button>
+          </div>
+          <DateRangePicker
+            ranges={selectedDateRange}
+            onChange={handleDateRangeChange}
+          />
+          <div className="flex justify-end">
+            <button onClick={closeModal}>Submit</button>
+          </div>
+        </Modal>
+      </div>
+
       <div className="flex flex-col sm:flex-row items-center gap-3 justify-between ">
         <div
           className="flex flex-col items-start gap-1 justify-around bg-gray-200 border rounded-lg p-2  w-full "
@@ -164,7 +273,7 @@ const LineChart = ({
             </div>
           </div>
           <div className="flex items-center gap-0.2 justify-around ">
-            <h3 className="text-base font-bold">{onlineStoreSessions}</h3>
+            <h3 className="text-base font-bold">{selectedDateRange == 0 ? onlineStoreSessions : filteredOnlineStoreSessions}</h3>
             <IoMdArrowDropup className="text-sm" />
             <h3 className="text-sm text-gray-400">9%</h3>
           </div>
@@ -224,7 +333,7 @@ const LineChart = ({
           </div>
 
           <div className="flex items-center gap-0.2">
-            <h2 className="text-l font-bold">$ {netReturnValue}</h2>,
+            <h2 className="text-l font-bold">{selectedDateRange == 0 ? `$ ${netReturnValue}` : `$ ${filteredTNetReturnValue}`}</h2>,
             <IoMdArrowDropup className="text-sm" />
             <h3 className="text-sm text-gray-400">14%</h3>
           </div>
@@ -283,7 +392,7 @@ const LineChart = ({
           </div>
 
           <div className="flex items-center gap-0.2">
-            <h2 className="text-l font-bold">{totalOrders}</h2>,
+            <h2 className="text-l font-bold">{selectedDateRange == 0  ? totalOrders : filteredTotalOrders}</h2>,
             <IoMdArrowDropup className="text-sm" />
             <h3 className="text-sm text-gray-400">2%</h3>
           </div>
@@ -344,7 +453,11 @@ const LineChart = ({
           </div>
 
           <div className="flex items-center gap-0.2">
-            <h2 className="text-l font-bold ">{conversionRate.toFixed(2)}{"%"}</h2>,
+            <h2 className="text-l font-bold ">
+              {selectedDateRange == 0  ? conversionRate.toFixed(2) : filteredConversionRate.toFixed(2)}
+              {"%"}
+            </h2>
+            ,
             <IoMdArrowDropup className="text-sm" />
             <h3 className="text-sm text-gray-400">7%</h3>
           </div>
@@ -394,54 +507,92 @@ const LineChart = ({
           )}
         </div>
       </div>
+
       {showChart ? (
         <div className=" p-3 m-2">
-          <ResponsiveContainer height={400}>
-            <AreaChart
-              data={dailyRevenue}
-              height={400}
-              margin={{
-                top: 10,
-                right: 30,
-                left: 0,
-                bottom: 0,
-              }}
-            >
-              <CartesianGrid strokeDasharray="0 0 0" />
-              <XAxis
-                dataKey="date"
-                tickCount={dailyRevenue?.data?.length ?? 0}
-                tick={{
-                  stroke: "light-grey",
-                  strokeWidth: 0.5,
-                  fontSize: "12px",
+          {selectedDateRange == 0 ? (
+            <ResponsiveContainer height={400}>
+              <AreaChart
+                data={dailyRevenue}
+                height={400}
+                margin={{
+                  top: 10,
+                  right: 30,
+                  left: 0,
+                  bottom: 0,
                 }}
-              />
-              <YAxis
-                tickCount={13}
-                tick={{
-                  stroke: "light-grey",
-                  strokeWidth: 0.5,
-                  fontSize: "12px",
-                }}
-                interval="preserveStartEnd"
-                domain={[0, "dataMax + 10"]}
-              />
+              >
+                <CartesianGrid strokeDasharray="0 0 0" />
+                <XAxis
+                  dataKey="date"
+                  tickCount={dailyRevenue?.length ?? 0}
+                  tick={{
+                    stroke: "light-grey",
+                    strokeWidth: 0.5,
+                    fontSize: "12px",
+                  }}
+                />
+                <YAxis
+                  tickCount={13}
+                  tick={{
+                    stroke: "light-grey",
+                    strokeWidth: 0.5,
+                    fontSize: "12px",
+                  }}
+                  interval="preserveStartEnd"
+                  domain={[0, "dataMax + 10"]}
+                />
 
-              <Area
-                type="monotone"
-                dataKey="value"
-                // stroke={colors?.stroke}
-                strokeWidth={3}
-                fill="none"
-                // fill={colors?.fill}
-                // dot={{
-                //     stroke: colors?.stroke,
-                //     strokeWidth: 3,
-                // }}
-              />
-            </AreaChart>
-          </ResponsiveContainer>
+                <Area
+                  type="monotone"
+                  dataKey="value"
+                  strokeWidth={3}
+                  fill="none"
+                />
+              </AreaChart>
+            </ResponsiveContainer>
+          ) : (
+            <ResponsiveContainer height={400}>
+              <AreaChart
+                data={filteredRevenueData}
+                height={400}
+                margin={{
+                  top: 10,
+                  right: 30,
+                  left: 0,
+                  bottom: 0,
+                }}
+              >
+                <CartesianGrid strokeDasharray="0 0 0" />
+                <XAxis
+                  dataKey="date"
+                  tickCount={filteredRevenueData?.length ?? 0}
+                  tick={{
+                    stroke: "light-grey",
+                    strokeWidth: 0.5,
+                    fontSize: "12px",
+                  }}
+                />
+                <YAxis
+                  tickCount={13}
+                  tick={{
+                    stroke: "light-grey",
+                    strokeWidth: 0.5,
+                    fontSize: "12px",
+                  }}
+                  interval="preserveStartEnd"
+                  domain={[0, "dataMax + 10"]}
+                />
+
+                <Area
+                  type="monotone"
+                  dataKey="value"
+                  strokeWidth={3}
+                  fill="none"
+                />
+              </AreaChart>
+            </ResponsiveContainer>
+          )}
         </div>
       ) : (
         ""
